57de938b23df497e8b545300b907735ed4707d4a
Mohammed Amer <99843245+mjamilamer@users.noreply.github.com>
2023-04-03 18:41:23 -0400

    basic outlining -- refactoring and notes within index pages

--- src/Yelp_clone/index.js
+++ src/Yelp_clone/index.js
@@ -1,11 +1,17 @@
+import React from 'react'
+import HomeScreen from "./HomeScreen";
+
 function Yelp_clone() {
     return (
         /**
          * Since there is a single landing page and multiple home screens
          * Landing page should be a separate component
          * */
-        <h2>LANDING PAGE</h2>
-        // Call landing page --
+        <>
+            <h2>LANDING PAGE</h2>
+            <HomeScreen/>
+        </>
+
     );
 }
 
