c3744f28efc0cfc8f3b16ec2d0c734b996b45342
alex-w-99 <awilcox99@gmail.com>
2023-04-24 13:54:20 -0400

    more minor changes

--- src/services/follow-service.js
+++ src/services/follow-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:4000/api";  // process.env.REACT_APP_API_BASE;
+const API_BASE = process.env.REACT_APP_API_BASE; //"http://localhost:4000/api";
 const FOLLOW_API = `${API_BASE}/follows`;
 const USERS_API = `${API_BASE}/users`;
 
