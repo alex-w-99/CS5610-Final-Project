0c4dedb09cd3e21c2601b23154fcf845b0660be8
Mohammed Amer <99843245+mjamilamer@users.noreply.github.com>
2023-04-26 01:07:29 -0400

    More configurations CSS and UMLS added to Readme

--- src/Components/selection/index.js
+++ src/Components/selection/index.js
@@ -1,72 +1,84 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import "./index.css"
 
-const CategoryActivitiesSelection = ({ activities }) => {
+const CategoryActivitiesSelection = ({activities}) => {
     const categories = [...new Set(activities.map(activity => activity.category))];
     const nav = useNavigate();
     const NavToSearch = (activity) => {
         nav(`/search/${activity}`, true);
     }
+    const ResToSearch = (activity) => {
+        nav(`/details/${activity}`, true);
+    }
+
+
     return (
-        <>
-            <div className="container">
-                <h2>Recent Activities</h2>
-                <div className="row">
-                    {activities.map((activity) => (
-                        <div className="col-md-4" key={activity.title}>
-                            <div className="card mb-4 shadow-sm">
-                                <img
-                                    className="card-img-top"
-                                    src={activity.imageUrl}
-                                    alt="Card cap"
-                                />
-                                <div className="card-body">
-                                    <p className="card-text">{activity.description}</p>
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <small className="text-muted">{activity.category}</small>
-                                        <div className="btn-group">
-                                            <button
-                                                type="button"
-                                                className="btn btn-sm btn-outline-secondary"
-                                                onClick={() => NavToSearch(activity.category)}
-                                            >
-                                                View
-                                            </button>
-                                            <button
-                                                type="button"
-                                                className="btn btn-sm btn-outline-secondary"
-                                            >
-                                                Edit
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
+        <div className="container">
+            <h2 className="text-center my-4">Recent Activities</h2>
+            <div className="row">
+                {activities.map((activity) => (
+                    <div className="col-lg-4 col-md-6 mb-4" key={activity.title}>
+                        <div className="card h-100">
+                            <img className="card-img-top img-fluid" src={activity.imageUrl}
+                                 alt="Card cap"/>
+                            <div className="card-body">
+                                <h4 className="card-title">{activity.title}</h4>
+                                <p className="card-text">{activity.description}</p>
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                                        <i className="bi bi-geo-alt"></i> Distance: {(activity.distance / 1609.344).toFixed(2)} mi
+                                    </li>
+
+                                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                                        <i className="bi bi-cash"></i> Price Range: {activity.price}
+                                    </li>
+                                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                                        <i className="bi bi-star-fill"></i> Rating: {activity.rating}
+                                    </li>
+                                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                                        <i className="bi bi-people"></i> Review
+                                        Count: {activity.review_count}
+                                    </li>
+                                </ul>
+                            </div>
+                            <div
+                                className="card-footer d-flex justify-content-between align-items-center">
+                                <button type="button" className="btn btn-sm btn-outline-secondary"
+                                        onClick={() => ResToSearch(activity.id)}>
+                                    <i className="bi bi-search"></i> View
+                                </button>
                             </div>
                         </div>
-                    ))}
-                </div>
-                <h2>Activity Categories</h2>
-                <div className="row">
-                    {categories.map((category) => (
-                        <div className="col-md-4" key={category}>
-                            <div className="card mb-4 shadow-sm">
-                                <div className="card-body">
-                                    <h5 className="card-title">{category}</h5>
-                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit arcu in erat tincidunt, nec rutrum risus sagittis. Nunc accumsan massa sed nisi tincidunt consequat. </p>
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <div className="btn-group">
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">View Activities</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit Category</button>
-                                        </div>
-                                        <small className="text-muted">{activities.filter(activity => activity.category === category).length} Activities</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                    </div>
+
+                ))}
             </div>
-        </>
+            <h2 className="text-center my-4">Activity Categories</h2>
+            <div className="row">
+                {categories.map((category) => (
+                    <div className="col-lg-4 col-md-6 mb-4" key={category}>
+                        <div className="card h-100">
+                            <div className="card-body">
+                                <h4 className="card-title">{category}</h4>
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit. Phasellus blandit arcu in erat tincidunt, nec
+                                    rutrum risus sagittis. Nunc accumsan massa sed nisi tincidunt
+                                    consequat.</p>
+                            </div>
+                            <div
+                                className="card-footer d-flex justify-content-between align-items-center">
+                                <button type="button" className="btn btn-sm btn-outline-secondary"
+                                        onClick={() => NavToSearch(category)}>View Activities
+                                </button>
+                                <small
+                                    className="text-muted">{activities.filter(activity => activity.category === category).length} Activities</small>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
 
--- src/navigation-bar/index.js
+++ src/navigation-bar/index.js
@@ -1,38 +1,107 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from "react-redux";
-import { logoutThunk } from "../services/users-thunks";
+import React from "react";
+import {Link, useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {logoutThunk} from "../services/users-thunks";
 
 const NavigationBar = () => {
-    const { currentUser } = useSelector((state) => state.users);
+    const {currentUser} = useSelector((state) => state.users);
 
     const dispatch = useDispatch();
     const nav = useNavigate();
     const logoutHandler = () => {
-        dispatch( logoutThunk() );
+        dispatch(logoutThunk());
         nav("/login");
     };
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
+                <div className="d-flex align-items-center">
+                    <Link to="/" className="navbar-brand ">
+                        <img
+                            src="/images/chews-wisely.png"
+                            width="50"
+                            height="50"
+                            className="d-inline-block  rounded-3"
+                            alt="Chews Wisely logo"
+                        />
+                    </Link>
 
-                <Link to="/" className="navbar-brand fw-bold">
-                    <img src="/images/chews-wisely.png"
-                         width="50" height="50"
-                         className="d-inline-block align-top rounded-3"
-                         style={ { marginLeft: "50px" } }
-                         alt="Chews Wisely logo" />
-                </Link>
-
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                </button>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                </div>
 
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto justify-content-evenly w-100">
+                        <li className="nav-item d-none d-lg-block">
+                            <Link className="nav-link" to="/home">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item d-none d-lg-block">
+                            <Link className="nav-link" to="/search/food">
+                                Restaurants
+                            </Link>
+                        </li>
 
+                        <li className="nav-item d-none d-lg-block">
+                            <Link className="nav-link" to="/about">
+                                About
+                            </Link>
+                        </li>
+
+                        {currentUser ? (
+                            <>
+                                <li className="nav-item d-none d-lg-block">
+                                    <Link className="nav-link" to="/profile">
+                                        <img
+                                            src={currentUser.profilePicture}
+                                            alt="profile"
+                                            height="25px"
+                                            width="25px"
+                                            style={{borderRadius: "50%"}}
+                                        />
+                                        &nbsp;
+                                        {currentUser.username}
+                                    </Link>
+                                </li>
+
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/" onClick={logoutHandler}>
+                                        Log Out
+                                    </Link>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className="nav-item d-none d-lg-block">
+                                    <Link className="nav-link" to="/login">
+                                        Login
+                                    </Link>
+                                </li>
+                            </>
+                        )}
+
+                        <li className="nav-item d-none d-lg-block">
+                            <button className="btn btn-dark btn-sm mt-1" type="submit">
+                                Write a Review
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* Mobile View */}
+                <div className="container-fluid d-lg-none">
+                    <ul className="navbar-nav ms-auto justify-content-between w-100">
                         <li className="nav-item">
                             <Link className="nav-link" to="/home">
                                 Home
@@ -45,31 +114,16 @@ const NavigationBar = () => {
                             </Link>
                         </li>
 
-                        <li className="nav-item">
-                            <Link className="nav-link" href="#">
-                                Restaurants
-                            </Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link className="nav-link" href="#">
-                                Search
-                            </Link>
-                        </li>
-
-                        { /* If logged in, must display "logout"; else, display "login" */ }
-                        {
-                            currentUser
-                            ?
+                        {currentUser ? (
                             <>
                                 <li className="nav-item">
-                                    <Link className="nav-link"
-                                          to="/profile">
+                                    <Link className="nav-link" to="/profile">
                                         <img
                                             src={currentUser.profilePicture}
                                             alt="profile"
-                                            height="25px" width="25px"
-                                            style={ { borderRadius: "50%" } }
+                                            height="25px"
+                                            width="25px"
+                                            style={{borderRadius: "50%"}}
                                         />
                                         &nbsp;
                                         {currentUser.username}
@@ -77,37 +131,26 @@ const NavigationBar = () => {
                                 </li>
 
                                 <li className="nav-item">
-                                    <Link className="nav-link"
-                                          to="/"
-                                          onClick={logoutHandler}>
+                                    <Link className="nav-link" to="/" onClick={logoutHandler}>
                                         Log Out
                                     </Link>
                                 </li>
                             </>
-                            :
+                        ) : (
                             <>
                                 <li className="nav-item">
-                                    <Link className="nav-link"
-                                          to="/login">
-                                        Profile
-                                    </Link>
-                                </li>
-
-                                <li>
-                                    <Link className="nav-link"
-                                          to="/login">
-                                        Log In
+                                    <Link className="nav-link" to="/login">
+                                        Login
                                     </Link>
                                 </li>
                             </>
-                        }
+                        )}
 
                         <li className="nav-item">
                             <button className="btn btn-dark btn-sm mt-1" type="submit">
                                 Write a Review
                             </button>
                         </li>
-
                     </ul>
                 </div>
             </div>
